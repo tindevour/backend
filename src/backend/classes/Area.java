74fbbd5b6e423b735d2af6a08cc641441fd369a2
Meet Mangukiya <meetmangukiya98@gmail.com>
2018-11-06 19:34:12 +0530

    classes/Area.java: Create Area class

---
+++ src/backend/classes/Area.java
@@ -0,0 +1,44 @@
+package backend.classes;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import backend.database.Database;
+import backend.classes.Common;
+
+
+public class Area implements Common{
+	public String area;
+	public int aid;
+	
+	public Area(String area, int aid) {
+		this.area = area;
+		this.aid = aid;
+	}
+	
+	public boolean isNotFound() { return false; }
+	
+	public static Area[] getAllAreas() {
+		try {
+			ResultSet rs = Database.executeQuery("select area, aid from areas");
+			rs.last();
+			int size = rs.getRow();
+			rs.beforeFirst();
+			Area[] areas = new Area[size];
+
+			int i = 0;
+			
+			while (rs.next()) {
+				String aname = rs.getString(1);
+				int aid = rs.getInt(2);
+				Area aobj = new Area(aname, aid);
+				areas[i] = aobj;
+			}
+			
+			return areas;
+		}
+		catch(SQLException ex) {
+			return new Area[]{};
+		}
+	}
+}
