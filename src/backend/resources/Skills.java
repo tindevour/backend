6d1b4b7d233c6be9505b4ccf33ff2a91185a23d4
Meet Mangukiya <meetmangukiya98@gmail.com>
2018-11-06 20:13:02 +0530

    resources/Skills: Implement GET /skills

--- src/backend/resources/Skills.java
+++ src/backend/resources/Skills.java
@@ -7,6 +7,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import backend.classes.Skill;
+import backend.resources.Utils;
+
 /**
  * Servlet implementation class Skills
  */
@@ -26,6 +29,7 @@ public class Skills extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO
+		Skill[] skills = Skill.getAllSkills();
+		Utils.jsonResponse(response, skills);
 	}
 }
